e7ea91445612909bbdf19cd28d192301872fcd96
SuhairShareef <suhairshareef1999@gmail.com>
2020-04-15 23:12:17 +0300

    creating the header navbar and sliding bar

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/brands.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>News Website</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class=" header col-md-12">
+                <div class="logo">
+                    <img id="logo" src="assets/img/shasha_logo.png" alt="">
+                </div>
+                <div class="ad-top">
+                    <img id="logo" src="assets/img/live.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light col-md-12">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-right">
+                        <li class="nav-item active">
+                            <a class="nav-link pull-right" href="#">الرئيسية<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">أهم الأخبار</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">عالمي وعربي</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">أخبار فلسطين</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                ترفيه
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">صحة وحياة</a>
+                                <a class="dropdown-item" href="#">علوم وتكنولوجيا</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">ادم وحواء</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">منوعات وفن</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">مقالات</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline mr-auto my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="البحث" aria-label="Search">
+                        <button class="btn btn-danger my-2 my-sm-1" type="submit">hhh
+                    </form>
+                </div>
+            </nav>
+        </div>
+
+        <div class="row marquee">
+            <div class="alert alert-danger alert-dismissible col-md-12" role="alert">
+                <strong>
+                    <marquee direction="right" truespeed = "20">
+                        – الرئيس يصدر توجيهاته بأهمية العمل الحثيث على تطبيق أحكام القانون وملاحقة المخالفين
+                        لتدابير الطوارئ
+                        &emsp; &emsp; &emsp; &emsp;
+                        – غيث: الاحتلال يضع العراقيل أمام أي عمل من طرفنا
+                        &emsp; &emsp; &emsp; &emsp;
+                        – تسجيل 13 إصابة جديدة بـ”كورونا”.. و31 وفاة في صفوف ابناء الجاليات الفلسطينية بالعالم
+                    </marquee>
+            </div>
+        </div>
+    </div>
+
+    <div class="row main-content">
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
