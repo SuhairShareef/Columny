c5babefde320ffae2657cc998ff63dabed52d362
SuhairShareef <suhairshareef1999@gmail.com>
2020-04-25 03:13:32 +0300

    Add news form

---
+++ adminPanel/addNews.php
@@ -0,0 +1,131 @@
+<?php
+session_start();
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0)
+{ 
+header('location:index.php');
+}
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Add News</title>
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+
+<body class="fixed-left">
+    <div id="wrapper">
+
+        <!-- Top Bar -->
+        <?php include('includes/topheader.php');?>
+
+        <!-- Left Sidebar -->
+        <?php include('includes/leftsidebar.php');?>
+
+        <!-- Content -->
+        <div class="content-page">
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="page-title-box">
+                                <h4 class="page-title">Add News</h4>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            <div class="p-6">
+                                <div class="">
+                                    <form action="addNewNews.php" name="addnews" method="post" enctype="multipart/form-data">
+                                        <div class="form-group m-b-20">
+                                            <label for="exampleInputEmail1">Title</label>
+                                            <input type="text" class="form-control" id="posttitle" name="posttitle"
+                                                placeholder="Enter title" required>
+                                        </div>
+                                        <div class="form-group m-b-20">
+                                            <label for="exampleInputEmail1">Category</label>
+                                            <select class="form-control" name="category" id="category"
+                                                onChange="getSubCat(this.value);" required>
+                                                <option value="">Category </option>
+                                                <?php
+                                                // Feching active categories
+                                                $query = "SELECT * FROM categories";
+                                                $result = mysqli_query($con,$query);
+
+                                                while ($category = mysqli_fetch_array($result))
+                                                {    
+                                                ?>
+                                                <option value = "<?php echo htmlentities($category['id']);?>">
+                                                    <?php echo htmlentities($category['name']);?></option>
+                                                <?php } ?>
+                                            </select>
+                                        </div>
+                                        <div class="form-group m-b-20">
+                                            <label for="exampleInputEmail1">Author's Name</label>
+                                            <input type="text" class="form-control" id="posttitle" name="posttitle"
+                                                placeholder="Enter name" required>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="card-box">
+                                                    <h4 class="m-b-30 m-t-0 header-title"><b>Content</b></h4>
+                                                    <textarea class="summernote" name="postdescription"
+                                                        required></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <div class="card-box">
+                                                    <h4 class="m-b-30 m-t-0 header-title"><b>Image</b></h4>
+                                                    <input type="file" class="form-control" id="postimage"
+                                                        name="postimage" required>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="submit" name="submit"
+                                            class="btn btn-success waves-effect waves-light">Save</button>
+                                        <button type="button"
+                                            class="btn btn-danger waves-effect waves-light">Discard</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    <!-- jQuery  -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/detect.js"></script>
+    <script src="assets/js/fastclick.js"></script>
+    <script src="assets/js/jquery.blockUI.js"></script>
+    <script src="assets/js/waves.js"></script>
+    <script src="assets/js/jquery.slimscroll.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/jquery.core.js"></script>
+    <script src="assets/js/jquery.app.js"></script>
+</body>
+</html>
--- adminPanel/categories.php
+++ adminPanel/categories.php
@@ -17,14 +17,14 @@ header('location:index.php');
 <head>
 
     <title>categories</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/myStyle.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/core.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/components.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/pages.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/menu.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+    <link href="assets/css/myStyle.css" rel="stylesheet" type="text/css"/>
 
 </head>
 
